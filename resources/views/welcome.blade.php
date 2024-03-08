7ffb9914298c41b7f1238e2c437c669a80d9284b
jijeyya <putricempaka2809@gmail.com>
2024-03-08 15:25:59 +0700

    tambah paragraf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,8 @@
     <body class="antialiased">
         <h1>HALLO</h1>
         <p>ini adalah percobaan commit</p>
-        <h1>paragraf</h1>
+        <h1 style="text">paragraf</h1>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut animi non laboriosam neque vitae vel officiis, libero esse. Quis itaque iste sequi ex aut! Aliquam soluta tenetur consectetur excepturi, facilis nobis, reprehenderit cumque non molestias eum assumenda culpa corporis voluptatum consequuntur suscipit illo voluptatem obcaecati dolores eos iure dolorem iste! Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, qui. Lorem, ipsum.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit quaerat pariatur possimus exercitationem ad eum reiciendis quisquam modi quidem voluptates optio est in itaque iure, repudiandae asperiores quos numquam? Saepe enim reprehenderit hic, distinctio ipsum earum cupiditate asperiores cumque aperiam ea ex fugiat nobis esse delectus tempore soluta eaque accusamus?
     </body>
 </html>
