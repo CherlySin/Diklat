5f71bf2e8cfd352b51791ee0684f59ada0ea9cc7
jijeyya <putricempaka2809@gmail.com>
2024-03-17 20:25:06 +0700

    edit db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_level');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->unique();
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('id_level')->references('id')->on('level')->onUpdate('cascade')
+            ->onDelete('cascade');
         });
     }
 
--- database/migrations/2024_03_17_121043_create_ajengs_table.php
+++ database/migrations/2024_03_17_121043_create_ajengs_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('level', function (Blueprint $table) {
+        Schema::create('ajengs', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_level');
+            $table->timestamps();
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('level');
+        Schema::dropIfExists('ajengs');
     }
 };
