183d0fc783bbab82affab21199d387dba408326c
cherly sintia <cherly.lilut@gmail.com>
2024-03-17 23:58:19 +0700

    landing

--- resources/views/utama/landingPage.blade.php
+++ resources/views/utama/landingPage.blade.php
@@ -1,5 +1,5 @@
 {{-- extend merupakan inheritance dari nama file yang ingin di warisi --}}
 @extends('layout/mainUser')
 @section('container')
-    lamding page, mencoba inheritace dari file main.
+    Welcome
 @endsection
