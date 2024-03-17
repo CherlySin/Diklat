abfe6e4203f43125a763a72249b3fcb282965e4d
jijeyya <putricempaka2809@gmail.com>
2024-03-17 12:29:31 +0700

    add database

---
+++ resources/views/utama/landingPage.blade.php
@@ -0,0 +1,5 @@
+{{-- extend merupakan inheritance dari nama file yang ingin di warisi --}}
+@extends('layout/mainUser')
+@section('container')
+    lamding page, mencoba inheritace dari file main.
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <title>Landing Page</title>
     </head>
     <body class="antialiased">
-        <h1>HALLO</h1>
+        <nav class="navbar navbar-dark bg-primary">
+            <!-- Navbar content -->
+          </nav>
         <p>ini adalah percobaan commit</p>
         <h1 style="text">paragraf</h1>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut animi non laboriosam neque vitae vel officiis, libero esse. Quis itaque iste sequi ex aut! Aliquam soluta tenetur consectetur excepturi, facilis nobis, reprehenderit cumque non molestias eum assumenda culpa corporis voluptatum consequuntur suscipit illo voluptatem obcaecati dolores eos iure dolorem iste! Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, qui. Lorem, ipsum.
